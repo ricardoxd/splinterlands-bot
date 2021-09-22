348ad8fee566291ef412e710f771b85d9c2f5eff
ricardoxd <chessdj@hotmail.com>
2021-09-22 17:37:52 -0400

    test

--- splinterlandsPage.js
+++ splinterlandsPage.js
@@ -1,6 +1,7 @@
 async function login(page, ACCOUNT, PASSWORD) {
     try {
         page.waitForSelector('#log_in_button > button').then(() => page.click('#log_in_button > button'))
+        page.waitForSelector('.pull-right').then(() => page.click('.pull-right'))  
         await page.waitForSelector('#email')
             .then(() => page.waitForTimeout(3000))
             .then(() => page.focus('#email'))
